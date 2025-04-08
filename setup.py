2a17caa90acf7dba6ec92b69f7d24fccef5e82e6
huangyusong <huangyusong@infoftt.onmicrosoft.com>
2025-04-08 10:57:30 +0800

    Chore: release tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai_chat_html_exporter",
-    version="1.0.0",
+    version="1.0.0.post1",
     packages=find_packages(),
     install_requires=[
         "langchain-core>=0.3.0",
